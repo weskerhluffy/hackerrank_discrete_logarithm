a6b4aa326625dad8feed8f4b10894e8f5c0d3530
ernesto.alvarado <ernesto.alvarado@adroll.com>
2019-08-29 20:46:00 -0500

    Initial Commit

---
+++ hackerrank_discrete_logarithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hackerrank_discrete_logarithm
+//
+//  Created by ernesto alvarado on 29/08/19.
+//  Copyright © 2019 ernesto alvarado. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
